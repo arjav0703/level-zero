7b831976ad6833358dfa841e9e9c7ed562ce5b27
Arjav Jain <arjavjain0703@gmail.com>
2026-03-20 00:17:47 +0530

    refactor section one code

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,3 +1,5 @@
+use crate::render::section_one::render_section_one;
+
 use super::App;
 use ratzilla::ratatui::{
     Frame,
@@ -11,6 +13,7 @@ use tui_big_text::{BigText, PixelSize};
 
 mod effects;
 pub use effects::*;
+mod section_one;
 
 impl App {
     pub fn render(&mut self, frame: &mut Frame, effect: &mut Effect) {
@@ -22,94 +25,10 @@ impl App {
         )
         .split(frame.area());
 
-        let vert_div = Layout::new(
-            Direction::Vertical,
-            [
-                Constraint::Percentage(20),
-                Constraint::Percentage(45),
-                Constraint::Percentage(35),
-            ],
-        )
-        .split(sections[0]);
-
-        draw_gauges(frame, self, vert_div[0]);
-
-        let horizontal_div = Layout::new(
-            Direction::Horizontal,
-            [
-                Constraint::Percentage(32),
-                Constraint::Min(20),
-                Constraint::Percentage(30),
-            ],
-        )
-        .split(vert_div[1]);
-
-        draw_pie_chart(frame, self, horizontal_div[0]);
-
-        let hero_vertical = Layout::new(
-            Direction::Vertical,
-            [
-                Constraint::Percentage(10),
-                Constraint::Percentage(80),
-                Constraint::Percentage(10),
-            ],
-        )
-        .split(horizontal_div[1]);
-
-        frame.render_widget(hero_text(), hero_vertical[1]);
-
-        draw_horizontal_barchart(frame, self, horizontal_div[2]);
-
-        let info_block = Block::bordered().title("INFO");
-
-        frame.render_widget(&info_block, vert_div[2]);
-
-        let inner_area = info_block.inner(vert_div[2]);
-        let info_vertical = Layout::new(
-            Direction::Vertical,
-            [
-                Constraint::Percentage(25),
-                Constraint::Percentage(50),
-                Constraint::Percentage(25),
-            ],
-        )
-        .split(inner_area);
-
-        frame.render_widget(info_text(), info_vertical[1]);
+        render_section_one(sections[0], frame, self);
 
         if effect.running() {
             frame.render_effect(effect, frame.area(), Duration::from_millis(100));
         }
     }
-}
-
-fn hero_text() -> impl Widget {
-    BigText::builder()
-        .alignment(ratzilla::ratatui::layout::HorizontalAlignment::Center)
-        .pixel_size(PixelSize::Full)
-        .style(Style::new().blue())
-        .lines(vec![
-            "level".light_red().into(),
-            "zero".white().into(),
-            "~~~~~".light_blue().into(),
-        ])
-        .build()
-}
-
-fn info_text() -> impl Widget {
-    BigText::builder()
-        .alignment(ratzilla::ratatui::layout::HorizontalAlignment::Center)
-        .pixel_size(PixelSize::Quadrant)
-        .style(Style::new().yellow())
-        .lines(vec![
-            "You ship -> A low level systems programming Project"
-                .light_cyan()
-                .into(),
-            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-                .cyan()
-                .into(),
-            "We Ship -> Cool computer hardware".light_green().into(),
-            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".green().into(),
-        ])
-        .build()
 }
---
+++ src/render/section_one.rs
@@ -0,0 +1,89 @@
+use super::*;
+
+pub fn render_section_one(area: Rect, frame: &mut Frame, app: &mut App) {
+    let vert_div = Layout::new(
+        Direction::Vertical,
+        [
+            Constraint::Percentage(20),
+            Constraint::Percentage(45),
+            Constraint::Percentage(35),
+        ],
+    )
+    .split(area);
+
+    draw_gauges(frame, app, vert_div[0]);
+
+    let horizontal_div = Layout::new(
+        Direction::Horizontal,
+        [
+            Constraint::Percentage(32),
+            Constraint::Min(20),
+            Constraint::Percentage(30),
+        ],
+    )
+    .split(vert_div[1]);
+
+    draw_pie_chart(frame, app, horizontal_div[0]);
+
+    let hero_vertical = Layout::new(
+        Direction::Vertical,
+        [
+            Constraint::Percentage(10),
+            Constraint::Percentage(80),
+            Constraint::Percentage(10),
+        ],
+    )
+    .split(horizontal_div[1]);
+
+    frame.render_widget(hero_text(), hero_vertical[1]);
+
+    draw_horizontal_barchart(frame, app, horizontal_div[2]);
+
+    let info_block = Block::bordered().title("INFO");
+
+    frame.render_widget(&info_block, vert_div[2]);
+
+    let inner_area = info_block.inner(vert_div[2]);
+    let info_vertical = Layout::new(
+        Direction::Vertical,
+        [
+            Constraint::Percentage(15),
+            Constraint::Percentage(70),
+            Constraint::Percentage(15),
+        ],
+    )
+    .split(inner_area);
+
+    frame.render_widget(info_text(), info_vertical[1]);
+}
+
+fn hero_text() -> impl Widget {
+    BigText::builder()
+        .alignment(ratzilla::ratatui::layout::HorizontalAlignment::Center)
+        .pixel_size(PixelSize::Full)
+        .style(Style::new().blue())
+        .lines(vec![
+            "level".light_red().into(),
+            "zero".white().into(),
+            "~~~~~".light_blue().into(),
+        ])
+        .build()
+}
+
+fn info_text() -> impl Widget {
+    BigText::builder()
+        .alignment(ratzilla::ratatui::layout::HorizontalAlignment::Center)
+        .pixel_size(PixelSize::Quadrant)
+        .style(Style::new().yellow())
+        .lines(vec![
+            "YS -> A low level systems programming project"
+                .light_cyan()
+                .into(),
+            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                .cyan()
+                .into(),
+            "WS -> Cool computer hardware".light_green().into(),
+            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~".green().into(),
+        ])
+        .build()
+}
